d7451fed370df4012cec84cc1c837ca4c84b335c
Aayushjn <aayushjn11@gmail.com>
2020-03-25 11:55:48 +0530

    Error returns more verbose message

--- app/errors.py
+++ app/errors.py
@@ -4,20 +4,28 @@ from app import app
 
 
 @app.errorhandler(400)
-def bad_request_error(error: str) -> Tuple[str, int]:
-    return error, 400
+def bad_request_error(error: str) -> Tuple[dict, int]:
+    return {
+               'message': error
+           }, 400
 
 
 @app.errorhandler(401)
-def unauthorized_error(error: str) -> Tuple[str, int]:
-    return error, 401
+def unauthorized_error(error: str) -> Tuple[dict, int]:
+    return {
+               'message': error
+           }, 401
 
 
 @app.errorhandler(404)
-def not_found_error(error: str) -> Tuple[str, int]:
-    return error, 404
+def not_found_error(error: str) -> Tuple[dict, int]:
+    return {
+               'message': error
+           }, 404
 
 
 @app.errorhandler(408)
-def request_timeout_error(error: str) -> Tuple[str, int]:
-    return error, 408
+def request_timeout_error(error: str) -> Tuple[dict, int]:
+    return {
+               'message': error
+           }, 408
